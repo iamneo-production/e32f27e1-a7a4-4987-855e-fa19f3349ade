6cd6fb0d1f9fbb91e471c3d7a393e835225e017b
s-deep3k <sankhadeep.genx@gmail.com>
2022-02-16 16:45:40 +0000

    Problem rendering tailwindcss

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { FormComponent } from './signup/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
