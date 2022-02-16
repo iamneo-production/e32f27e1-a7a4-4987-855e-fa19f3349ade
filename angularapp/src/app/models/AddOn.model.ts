400c9829abd36082edd4c9c91522a047d4ab246c
s-deep3k <sankhadeep.genx@gmail.com>
2022-02-16 15:27:54 +0000

    Added signup component(frontend) and all model  classes

---
+++ angularapp/src/app/models/AddOn.model.ts
@@ -0,0 +1,6 @@
+export class AddonModel {
+  private addonId!: number;
+  private addonName!: string;
+  private addonDescription!: string;
+  private addAddonPrice!: string;
+}
---
+++ angularapp/src/app/models/Admin.model.ts
@@ -0,0 +1,6 @@
+export class AdminModel {
+  private userRole!: string;
+  private email!: string;
+  private password!: string;
+  private mobileNumber!: string;
+}
