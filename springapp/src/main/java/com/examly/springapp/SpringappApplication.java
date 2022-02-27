22c88a4aa60186478f16c6da0319629e686f8132
vikkshdotsoni <vikkshdotsoni@gmail.com>
2022-02-27 08:52:35 +0000

    merge with sankhadeep

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -2,16 +2,12 @@ package com.examly.springapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
-public class SpringappApplication implements CommandLineRunner{
+public class SpringappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringappApplication.class, args);
 	}
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Starting Code");
-	}
+
 }
