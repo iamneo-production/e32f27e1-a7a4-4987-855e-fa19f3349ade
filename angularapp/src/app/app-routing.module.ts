951d59650127a4e0db7422411dd4916f1b5a4b28
PavithraSettipalli08 <spavithra2001@gmail.com>
2022-03-21 15:16:18 +0000

    Admin Routing Added

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminAddAddonsComponent } from './admin-addons/admin-add-addons/admin-add-addons.component';
+import { AdminAddonsComponent } from './admin-addons/admin-addons.component';
 import { AdminComponent } from './dashboard/admin/admin.component';
 import { UserComponent } from './dashboard/user/user.component';
 import { LoginComponent } from './login/login.component';
@@ -7,26 +9,44 @@ import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   {
-    path:'',
-    component:LoginComponent,
-    pathMatch:'full'
+    path: '',
+    component: LoginComponent,
+    pathMatch: 'full'
   },
   {
-    path:'signup',
-    component:SignupComponent,
-    pathMatch:'full'
+    path: 'signup',
+    component: SignupComponent,
+    pathMatch: 'full'
   },
   {
-    path:'admin-dashboard',
-    component:AdminComponent,
-    pathMatch:'full'
+    path: 'admin-dashboard',
+    component: AdminComponent,
+    pathMatch: 'full'
   },
   {
-    path:'user-dashboard',
-    component:UserComponent,
-    pathMatch:'full'
+    path: 'user-dashboard',
+    component: UserComponent,
+    pathMatch: 'full'
   },
+  {
+    path: 'admin',
+    children: [
+      {
+        path: 'addons',
+        component: AdminAddonsComponent
+      },
 
+      {
+        path: 'addAdd-ons',
+        component: AdminAddAddonsComponent
+      },
+      {
+        path: 'saveAddons',
+        component: AdminAddonsComponent
+      }
+
+    ]
+  }
 ];
 
 @NgModule({
