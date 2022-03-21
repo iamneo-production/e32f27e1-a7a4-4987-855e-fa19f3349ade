18af2ba2e79af55caff52b0ee04f7d4f8fb18e1b
PavithraSettipalli08 <spavithra2001@gmail.com>
2022-03-21 15:38:08 +0000

    AddOn And Assets Added

--- angularapp/src/app/admin-addons/admin-add-addons/admin-add-addons.component.ts
+++ angularapp/src/app/admin-addons/admin-add-addons/admin-add-addons.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { AddonModel } from 'src/app/models/AddOn.model';
+import { AddonService } from 'src/app/services/addon.service';
 
 @Component({
   selector: 'app-admin-add-addons',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminAddAddonsComponent implements OnInit {
 
-  constructor() { }
+  addOn : AddonModel = new AddonModel();
+
+  addOnForm : FormGroup;
+
+  constructor(private addOnService: AddonService) { }
 
   ngOnInit(): void {
+    this.initializeValues()
+  }
+
+  initializeValues(){
+    //TODO initialize addOn in case of update
+
+    
+    this.addOnForm = new FormGroup({
+      name: new FormControl(''),
+      description: new FormControl(''),
+      addOnPrice: new FormControl(''),
+      imageUrl: new FormControl(''),
+    });
+  }
+
+  submit(){    
+    this.addOnService.saveAddOn(this.addOn).subscribe((response: any) => {
+      console.log('Saved successfully', response)
+      this.addOnForm.reset()
+      //TODO display snack bar for saved message and remove alert
+      alert('Saved successfully')
+    },
+    (error) => {
+      //TODO display snack bar for error saving details
+    })
   }
 
 }
